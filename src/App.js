aea1887ae75d820e332ba8c4716b37df468f9e2a
emory623 <dvanullnulldva@gmail.com>
2022-07-11 23:32:32 +0300

    meet to state; managed components(syncing)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('введите текст...')
+
+  const increment = () =>{setLikes(likes + 1)};
+  const decrement = () =>{setLikes(likes - 1)};
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Likes: {likes}</h1>
+      <h2>{value}</h2>
+      <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
+      <button onClick= {increment}>increment</button>
+      <button onClick= {decrement}>decrement</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <div>app works</div>
-  </React.StrictMode>
+    <App />
 );
 
 reportWebVitals();
