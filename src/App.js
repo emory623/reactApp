fc90532dee23f00abb1622c497ecf8cbd8ebac9b
emory623 <dvanullnulldva@gmail.com>
2022-07-13 23:29:10 +0300

    first Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React, {useState} from 'react';
-import './App.css';
+import React from 'react';
+import Counter from './components/Counter'
 
-function App() {
-
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState('введите текст...')
-
-  const increment = () =>{setLikes(likes + 1)};
-  const decrement = () =>{setLikes(likes - 1)};
-
-
+const App = () => { 
   return (
     <div className="App">
-      <h1>Likes: {likes}</h1>
-      <h2>{value}</h2>
-      <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
-      <button onClick= {increment}>increment</button>
-      <button onClick= {decrement}>decrement</button>
+      <Counter/>
     </div>
   );
 }
