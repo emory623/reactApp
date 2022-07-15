92c66bbb8d1af2097d3b10dc70beadeed4e40da9
emory623 <dvanullnulldva@gmail.com>
2022-07-15 13:35:18 +0300

    add first class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Counter from './components/Counter'
+import ClassCounter from './components/ClassCounter'
 
 const App = () => { 
   return (
     <div className="App">
       <Counter/>
+      <ClassCounter/>
     </div>
   );
 }
