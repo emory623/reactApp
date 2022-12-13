d6788ffa403cbb35ca255bc9675622ea0a0a4a75
emory623 <dvanullnulldva@gmail.com>
2022-12-13 19:56:42 +0300

    made decomposition and create new hooks(usePosts.js)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PostForm from './components/PostForm/PostForm';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/MyButton/MyButton';
+import { usePosts } from './hooks/usePosts';
 
 
 
@@ -22,18 +23,7 @@ const App = () => {
 
   const [filter, setFilter] = useState({sort:'', query: ''})
 
-
-  const sortedPosts = useMemo(() => {
-    console.log("welldone")
-    if(filter.sort){
-      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo( ()=>{
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
