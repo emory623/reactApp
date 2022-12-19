ef2be20acc4796569d1f71d0cdaa76d3db7e7537
emory623 <dvanullnulldva@gmail.com>
2022-12-19 13:38:15 +0300

    react-router-dom. dont work...

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Приложение было создано 19.12.2022 в целях тестирования навыков.
+        </h1>
+    );
+};
+
+export default About;
